4d2375579f5f2319692b9e8fd555e74beeb1f16d
akazuko <sharma.ayush1995@gmail.com>
2018-01-15 17:33:33 +0530

    UI : Changed from extJS to materializecss.

--- portal/website/static/website/app.js
+++
@@ -1,5 +0,0 @@
-Ext.application({
-    name: 'PlacementPortal',
-    appFolder : '/static/website',
-    extend: 'PlacementPortal.Application'
-});
--- portal/website/views.py
+++ portal/website/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def index(request):
-    return render_to_response('website/index.html')
+    return render_to_response('website/login.html')
