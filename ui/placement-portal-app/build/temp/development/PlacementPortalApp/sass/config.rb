ccd2be7fbe54279d5e75421c3cd5644e0db7c591
Ayush Sharma <ayush.sharma@Ayushs-MacBook-Pro.local>
2018-10-20 14:14:15 +0530

    Adding placement-portal-sencha app

---
+++ ui/placement-portal-app/build/temp/development/PlacementPortalApp/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/ayush.sharma/Workspace/PlacementPortal/ui/placement-portal-app/build/.sass-cache'
---
+++ ui/placement-portal-app/build/temp/development/PlacementPortalApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/ayush.sharma/Workspace/PlacementPortal/ui/placement-portal-app/app.js
