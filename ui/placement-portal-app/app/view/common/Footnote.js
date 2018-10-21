544aa938b9786f05a66cfb90962a8954bcc926df
akazuko <sharma.ayush1995@gmail.com>
2018-10-21 10:50:09 +0530

    Adding the login page ( Update 1 )

--- ui/placement-portal-app/app/view/common/Footnote.js
+++ ui/placement-portal-app/app/view/common/Footnote.js
@@ -1,33 +1,5 @@
 Ext.define('PlacementPortalApp.view.common.Footnote', {
   extend: 'Ext.form.Panel',
   xtype: 'app-footnote',
-
-  requires: [
-    'Ext.Panel',
-  ],
-
-  layout: 'hbox',
-  
-  items: [{
-    flex: 6,
-    xtype: 'panel'
-  },{
-    flex:1,
-    xtype: 'panel',
-    layout: 'hbox',
-    items:[{
-      style: {
-        'padding-right': '5px'
-      },
-      html: 'Made with'
-    }, {
-      style: {
-        color: "red",
-        'padding-right': '5px'
-      },
-      html: '<i class="fa fa-heart" style="color:red"></i>'
-    }, {
-      html: 'by  <a href="akazuko.github.io" target="blank">Ayush Sharma</a>'
-    }]
-  }]
+  html: '<div align="right">Made with <i class="fa fa-heart" style="color:red"></i> by  <a href="akazuko.github.io" target="blank">Ayush Sharma</a><div>'
 });
